4c4119f840943d68132d67be11d4b7f3ec79a6ad
Dino Musić <dmusic996@gmail.com>
2023-08-31 19:21:58 +0200

    Verzija 1

---
+++ script.js
@@ -0,0 +1,26 @@
+const rootNode = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootNode)
+
+function App() {
+   // return React.createElement("h1", null, "Hello React")
+   React.createElement("div", null,
+    React.createElement("h1", null, "Hello React",
+        React.createElement("p", null, "primjer paragrafa"),
+        React.createElement("ul", null,),
+        React.createElement("li", null, "Subtitle 1"),
+        React.createElement("li", null, "Subtitle 2"),
+        React.createElement("li", null, "Subtitle 3"),
+    )
+    )
+
+}
+
+// const virtualDOM = React.createElement("h1", null, "Hello React")
+// console.log(virtualDOM)
+root.render(React.createElement(App))
+
+// const VD = {
+//    type: "h1",
+//    children: "Hello React"
+// }
